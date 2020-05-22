841223e71fde10d3697710dcdb501a762ac14620
armin <armin65@gmail.com>
2020-05-22 13:49:46 +0200

    Add notToEqual for expect

--- core/inc/Expect.h
+++ core/inc/Expect.h
@@ -1,16 +1,86 @@
 #pragma once
 
+#include <iostream>
+#include <sstream>
 #include <string>
+
+template <typename T> struct is_string { static const bool value = false; };
+
+template <class T, class Traits, class Alloc>
+struct is_string<std::basic_string<T, Traits, Alloc>> {
+  static const bool value = true;
+};
+
+template <typename T> struct is_printable {
+  static const bool value = is_string<T>::value || std::is_pointer<T>::value;
+};
 
 namespace entw {
 template <typename T> class Expect {
 public:
   explicit Expect(const T &a) : a_(a){};
 
+  using DummyType = void;
+
+  template <typename U = T,
+            typename std::enable_if<!std::is_arithmetic<U>::value &&
+                                        !is_printable<U>::value,
+                                    DummyType>::type * = nullptr>
+  std::string what(const U &expected, const U &actual) {
+    return std::string("expected equal values received non equal values");
+  }
+
+  template <typename U = T,
+            typename std::enable_if<!std::is_arithmetic<U>::value &&
+                                        !is_printable<U>::value,
+                                    DummyType>::type * = nullptr>
+  std::string notWhat(const U &value) {
+    return std::string("expected non equal values received equal values");
+  }
+
+  template <typename U = T,
+            typename std::enable_if<std::is_arithmetic<U>::value,
+                                    DummyType>::type * = nullptr>
+  std::string what(const U &expected, const U &actual) {
+    return std::string("expected\n " + std::to_string(expected) +
+                       "\nreceived\n " + std::to_string(actual));
+  }
+
+  template <typename U = T,
+            typename std::enable_if<is_printable<U>::value, DummyType>::type * =
+                nullptr>
+  std::string what(const U &expected, const U &actual) {
+    std::stringstream result;
+    result << "expected\n " << expected << "\nreceived\n " << actual;
+    return result.str();
+  }
+
+  template <typename U = T,
+            typename std::enable_if<std::is_arithmetic<U>::value,
+                                    DummyType>::type * = nullptr>
+  std::string notWhat(const U &value) {
+    return std::string("expected different values\nreceived equal values\n" +
+                       std::to_string(value));
+  }
+
+  template <typename U = T,
+            typename std::enable_if<is_printable<U>::value, DummyType>::type * =
+                nullptr>
+  std::string notWhat(const U &value) {
+    std::stringstream result;
+    result << "expected different values\nreceived equal values\n" << value;
+    return result.str();
+  }
+
+  void notToEqual(const T &a) noexcept(false) {
+    if (!(this->a_ != a)) {
+      throw std::logic_error(notWhat(a));
+    }
+  }
+
   void toEqual(const T &a) noexcept(false) {
-    if (this->a_ != a) {
-      throw std::logic_error("expected\n " + std::to_string(a) + "\nreceived\n " +
-                             std::to_string(this->a_));
+    if (!(this->a_ == a)) {
+      throw std::logic_error(what(a, this->a_));
     }
   }
 
@@ -18,12 +88,6 @@ private:
   T a_;
 };
 
-template <typename T> Expect<T> expect(const T &a) { return Expect<T>{a}; };
+template <typename T> Expect<T> expect(const T &a) { return Expect<T>{a}; }
 
-template <> void Expect<std::string>::toEqual(const std::string &a) {
-  if (this->a_ != a) {
-    throw std::logic_error(
-        std::string("expected\n " + a + "\nreceived\n " + this->a_));
-  }
-}
 } // namespace entw
--- test/ExpectTestCase.h
+++ test/ExpectTestCase.h
@@ -7,30 +7,105 @@ namespace entw {
 class ExpectTestCase : public TestCase {
 public:
   void include() override {
-    it("expect_success_for_equal_number", [&]() {
-      expect<int>(50).toEqual(50);
-    });
+    it("expects success for equal number", [&]() { expect(50).toEqual(50); });
 
-    it("expect_failure_for_non_equal_numbers", [&]() {
+    it("expects failure for non equal numbers", [&]() {
       try {
-        expect<int>(50).toEqual(51);
-      }
-      catch (const std::exception& e) {
-        assert(std::string(e.what()) == std::string("expected\n 51\nreceived\n 50"));
+        expect(50).toEqual(51);
+      } catch (const std::exception &e) {
+        assert(std::string(e.what()) ==
+               std::string("expected\n 51\nreceived\n 50"));
       }
     });
 
-    it("expect_success_for_equal_string", [&]() {
-      expect<std::string>("test").toEqual("test");
-    });
+    it("expects success for equal string",
+       [&]() { expect(std::string("test")).toEqual(std::string("test")); });
 
-    it("expect_failure_for_non_equal_string", [&]() {
+    it("expects failure for non equal string", [&]() {
       try {
-        expect<std::string>("badString").toEqual("goodString");
+        expect(std::string("badString")).toEqual(std::string("goodString"));
+      } catch (const std::exception &e) {
+        assert(std::string(e.what()) ==
+               "expected\n goodString\nreceived\n badString");
       }
-      catch (const std::exception& e) {
-        assert(std::string(e.what()) == "expected\n goodString\nreceived\n badString");
+    });
+
+    it("expects success equal pointers", [&]() {
+      class Cat {
+        int meow = 0;
+      };
+
+      const auto data = Cat();
+      const auto pointerToCat = &data;
+
+      expect(&data).toEqual(pointerToCat);
+    });
+
+    it("expects fail with non equal pointers", [&]() {
+      class Cat {
+        int meow = 0;
+      };
+
+      const auto first_cat = Cat();
+      const auto second_cat = Cat();
+
+      try {
+        expect(&first_cat).toEqual(&second_cat);
+      } catch (const std::exception &e) {
+
+        assert(std::string(e.what()).find("expected") != std::string::npos);
+        assert(std::string(e.what()).find("received") != std::string::npos);
       }
+    });
+
+    it("supports not semantics", [&]() {
+      expect(50).notToEqual(51);
+    });
+
+    it("expects failure for equal numbers", [&]() {
+      try {
+        expect(50).notToEqual((50));
+      } catch (const std::exception &e) {
+        assert(std::string(e.what()) ==
+            std::string("expected different values\nreceived equal values\n50"));
+      }
+    });
+
+    it("expects failure for equal strings", [&]() {
+      try {
+        expect(std::string("test")).notToEqual((std::string("test")));
+      } catch (const std::exception &e) {
+        assert(std::string(e.what()) ==
+            std::string("expected different values\nreceived equal values\ntest"));
+      }
+    });
+
+    it("expects success with equal objects", [&]() {
+      class Comparable {
+      public:
+        bool operator == (const Comparable& comp)
+        {
+          return true;
+        }
+      };
+
+      const auto comparable = Comparable();
+
+      expect(comparable).toEqual(comparable);
+    });
+
+    it("expects success with non equal objects", [&]() {
+      class Comparable {
+      public:
+        bool operator != (const Comparable& comp)
+        {
+          return true;
+        }
+      };
+
+      const auto comparable = Comparable();
+
+      expect(comparable).notToEqual(comparable);
     });
   }
 };
